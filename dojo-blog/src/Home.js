14d27559992e36322c999578de12895e98bff3a1
Wall'e <duanqingbb@163.com>
2024-07-07 09:08:43 +0800

    add useEffect demonstration

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -7,10 +7,13 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
+
+    useEffect( () => console.log(blogs) )
     const handleDelete = (id) => {
         const newBlogs = blogs.filter( (blog) => blog.id !== id )
         setBlogs(newBlogs)
     }
+
      
     return ( 
         <div className="home">
