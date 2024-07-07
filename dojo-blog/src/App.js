350fd6d4936981a8a5705573d98e62c317f0191f
Wall'e <duanqingbb@163.com>
2024-07-07 13:59:18 +0800

    add Router Link demo

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import Navbar from "./Navbar";
 import Home from "./Home";
 import Create from "./Create";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
   const title = "Welcome to the new blog";
