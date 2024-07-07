8da98899e59f7501735c5026b3d49be0422e641f
Wall'e <duanqingbb@163.com>
2024-07-07 08:47:11 +0800

    add function as props' value

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,11 +1,12 @@
-const BlogList = (props) => {
-    const blogs = props.blogs
+const BlogList = ({blogs, handleDelete}) => {
+    const hasDelete = handleDelete !== undefined
     return (
         <div className="blog-list">
             {blogs.map(blog => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Written by {blog.author}</p>
+                    { hasDelete ? (<button onClick={() => handleDelete(blog.id)}>delete</button>) : "" }
                 </div>
             ))}
         </div>
