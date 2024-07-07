f48f65cd2ade52f6c3bdd370d18476e4bdeb5815
Wall'e <duanqingbb@163.com>
2024-07-07 18:40:05 +0800

    demo for making a post request

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,37 +1,55 @@
 import { useState } from "react";
 
 const Create = () => {
-    const [title, setTitle] = useState('');
-    const [body, setBody] = useState('');
-    const [author, setAuthor] = useState('mario')
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
 
+  const handleSubmit = (e) => {
     const blog = {
-        title: title,
-        body: body,
-        author: author,
-    }
-    const submitEvent = (e) => {
-        e.preventDefault()
-        console.log(blog)
-    }
+      title: title,
+      body: body,
+      author: author,
+    };
+    e.preventDefault();
+    fetch("http://localhost:8000/blogs/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added");
+    });
+    console.log(blog);
+  };
 
-    return ( 
-        <div className="create">
-            <h2>Add a New Blog</h2>
-            <form action="" onSubmit={submitEvent}>
-                <label htmlFor="">Blog title:</label>
-                <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
-                <label htmlFor="">Blog body:</label>
-                <textarea required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
-                <label htmlFor="">Blog author:</label>
-                <select value={author} onChange={(e) => setAuthor(e.target.value)}>
-                    <option value="mario">mario</option>
-                    <option value="yoshi">yoshi</option>
-                </select>
-                <button>Add Blog</button>
-            </form>
-        </div>
-    );
-}
- 
-export default Create
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form action="" onSubmit={handleSubmit}>
+        <label htmlFor="">Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label htmlFor="">Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label htmlFor="">Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
+    </div>
+  );
+};
+
+export default Create;
