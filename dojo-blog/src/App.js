a8a188ab4de0e57ff37158a0ea3c68c6e1e9dfef
Wall'e <duanqingbb@163.com>
2024-07-07 12:34:04 +0800

    add React Router demo

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,17 +1,25 @@
-import logo from './logo.svg';
-import Navbar from './Navbar';
-import Home from './Home';
+import logo from "./logo.svg";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   const title = "Welcome to the new blog";
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar/>
-        {title}
-        <Home/>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Navbar />
+          <div className="content">
+            <Switch>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </header>
+      </div>
+    </Router>
   );
 }
 
