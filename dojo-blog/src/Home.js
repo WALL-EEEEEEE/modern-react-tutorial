315a9dd1f8619c4975a04bd9422a1b1edb7f363f
Wall'e <duanqingbb@163.com>
2024-07-07 09:24:31 +0800

    add useEffect dependency demo

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -7,17 +7,24 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
+    const [name, setName] = useState("mario")
 
-    useEffect( () => console.log(blogs) )
+    useEffect( () => { 
+        console.log("use effect")
+        console.log(name)
+    }, [name])
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter( (blog) => blog.id !== id )
         setBlogs(newBlogs)
     }
 
-     
+
     return ( 
         <div className="home">
             <BlogList blogs={blogs} handleDelete={handleDelete}/>
+            <p > {name} </p>
+            <button onClick={ ()  => {setName("luigi")}}> change name</button>
         </div>
      );
 }
