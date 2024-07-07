1b15c607154a283285da751c6a9470416e9aabd3
Wall'e <duanqingbb@163.com>
2024-07-07 15:51:22 +0800

    add useEffect cleanup demo

--- dojo-blog/src/useFetch.js
+++ dojo-blog/src/useFetch.js
@@ -1,27 +1,40 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
 const useFetch = (url) => {
-   const [data, setData ]  = useState(null)
-    const [isLoading, setIsLoading] = useState(true)
-    const [error, setError] = useState(null)
-
-    useEffect( () => { 
-        fetch(url).then((res) => {
-            if (!res.ok)  {
-                setIsLoading(false)
-                throw Error(`failed to get resources from ${url}`)
-            }
-            return res.json()
-        }).then((data) => {
-            setData(data)
-            setIsLoading(false)
-        }).catch((err) => {
-            console.error(err)
-            setError(err)
-            setIsLoading(false)
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const abortCont = new AbortController();
+    setTimeout(() => {
+      fetch(url, { signal: abortCont.signal })
+        .then((res) => {
+          if (!res.ok) {
+            setIsLoading(false);
+            throw Error(`failed to get resources from ${url}`);
+          }
+          return res.json();
         })
-    }, [url])
-    return {data, isLoading, error};
-}
- 
+        .then((data) => {
+          setData(data);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          if (err.name == "AbortError") {
+            console.log("abort");
+          } else {
+            console.error(err);
+            setError(err);
+            setIsLoading(false);
+          }
+        });
+    }, 1000);
+
+    return () => {
+      abortCont.abort();
+    };
+  }, [url]);
+  return { data, isLoading, error };
+};
+
 export default useFetch;
