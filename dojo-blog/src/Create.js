2a46667d09e9f3908ab0a969932c75070ffa4031
Wall'e <duanqingbb@163.com>
2024-07-07 17:56:10 +0800

    add Controlled Inputs demo

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,6 +1,25 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario')
     return ( 
-        <div className="createBlog"> create a new Blog</div> 
+        <div className="create">
+            <h2>Add a New Blog</h2>
+            <form action="">
+                <label htmlFor="">Blog title:</label>
+                <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
+                <label htmlFor="">Blog body:</label>
+                <textarea required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+                <label htmlFor="">Blog author:</label>
+                <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
+        </div>
     );
 }
  
