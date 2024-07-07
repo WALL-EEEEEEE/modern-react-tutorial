2f58cd2954df02cdb73ccac02de810f9897583c6
Wall'e <duanqingbb@163.com>
2024-07-07 16:46:32 +0800

    reuse useFetch

--- dojo-blog/src/BlogDetails.js
+++ dojo-blog/src/BlogDetails.js
@@ -1,12 +1,31 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
-    const { id } = useParams()
-    return ( 
+  const { id } = useParams();
+  const {
+    data: blog,
+    isLoading,
+    error,
+  } = useFetch(`http://localhost:8000/blogs/${id}`);
+  return (
+    <div>
+      {isLoading && <div>Loading</div>}
+      {error && <div>{error.message}</div>}
+
+      {blog && (
         <div className="blog-details">
-            <h2>Blog details - { id } </h2>
+          <h2> {blog.title} </h2>
+          <article>
+            <h2>written by {blog.author}</h2>
+            <div>
+                {blog.body}
+            </div>
+          </article>
         </div>
-     );
-}
- 
+      )}
+    </div>
+  );
+};
+
 export default BlogDetails;
